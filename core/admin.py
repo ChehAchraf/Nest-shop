773de6e2039dc5699edbe25d751705770798eaa9
ChehAchraf <achrafchehboun@outlook.com>
2025-06-10 17:05:50 +0100

    update some app

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['name','phone']
+    search_fields = ['name','phone']
+    list_filter = ['name','phone']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title','image']
+    search_fields = ['title','image']
+    list_filter = ['title','image']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','image']
+    search_fields = ['title','image']
+    list_filter = ['title','image']
+
+class CartOrderAdmin(admin.ModelAdmin):
+    list_display = ['user','price']
+    search_fields = ['user','price']
+    list_filter = ['user','price']
+
+
+
+
+
+
+
+
+
+
+admin.site.register(vendor,VendorAdmin)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(CartOrder,CartOrderAdmin)
+admin.site.register(ProductImage)
+
--- core/models.py
+++ core/models.py
@@ -2,14 +2,41 @@ from django.db import models
 from shortuuid.django_fields import ShortUUIDField
 from django.utils.html import mark_safe
 from django.contrib.auth.models import User
+from django.conf import settings
 # Create your models here.
+
+
+STATUS_CHOICES = (
+    ("proccessing","Proccessing"),
+    ("shipped","Shipped"),
+    ("delivred","Delivred"),
+)
+
+STATUS = (
+    ("draft","Draft"),
+    ("disabled","DIsabled"),
+    ("rejected","Rejected"),
+    ("in_review","In Review"),
+    ("published","Published"),
+)
+
+RATING = (
+    (1,"⭐"),
+    (2,"⭐⭐"),
+    (3,"⭐⭐⭐"),
+    (4,"⭐⭐⭐⭐"),
+    (5,"⭐⭐⭐⭐⭐"),
+)
+
+
+
 
 def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 
 class Category(models.Model):
-    cid = ShortUUIDField(unique=True, lenght=10,max_length=30 , prefix="cat",alphabet="abcdefgh12345")
+    cid = ShortUUIDField(unique=True, length=10,max_length=30 , prefix="cat",alphabet="abcdefgh12345")
     title = models.CharField(max_length=120)
     image = models.ImageField(upload_to="category")
     
@@ -24,11 +51,11 @@ class Category(models.Model):
         return self.title
     
     
-class vendor(models.model):
-    vid = ShortUUIDField(unique=True, lenght=10,max_length=30 , prefix="ven",alphabet="abcdefgh12345")
+class vendor(models.Model):
+    vid = ShortUUIDField(unique=True, length=10,max_length=30 , prefix="ven",alphabet="abcdefgh12345")
     name = models.CharField(max_length=120)
     image = models.ImageField(upload_to=user_directory_path)
-    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True) 
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True) 
     description = models.TextField(blank=True)
     address = models.TextField(blank=True)
     chat_resp_time = models.CharField(max_length=120, blank=True,default="24 hours")
@@ -39,8 +66,95 @@ class vendor(models.model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    def vendor_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+    
     class Meta:
         verbose_name = 'vendor'
         verbose_name_plural = 'vendors'
         
+    
+        
+class Tags(models.Model):
+    pass
+        
+class Product(models.Model):
+    pid = ShortUUIDField(unique=True, length=10,max_length=30 , prefix="pro",alphabet="abcdefgh12345")
+    title = models.CharField(max_length=120)
+    image = models.ImageField(upload_to=user_directory_path)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    old_price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    in_stock = models.BooleanField(default=True)
+    feutured = models.BooleanField(default=False)
+    digital = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    specification = models.TextField(blank=True,null=True)
+    tags = models.ManyToManyField(Tags, blank=True)
+    product_status = models.CharField(choices=STATUS,max_length=10,default='in_review')
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    sku = ShortUUIDField(unique=True, length=10,max_length=30 , prefix="sku",alphabet="abcdefgh12345")
+    
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+        
+    def product_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+    
+    def __str__(self):
+        return self.title
+    
+    def get_percentage(self):
+        new_price = (self.old_price - self.price)  * 100
+        return new_price
+    
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images')
+    date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'product image'
+        verbose_name_plural = 'product images'
+        
+
+class CartOrder(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    paid_status = models.BooleanField(default=False)
+    order_date = models.DateTimeField(auto_now_add=True)
+    product_status = models.CharField(choices=STATUS_CHOICES,max_length=30,default='proccessing')
+    
+    class Meta:
+        verbose_name = 'cart order'
+        verbose_name_plural = 'cart orders'
+        
+    def __str__(self):
+        return self.user.username
+    
+class CartOrderItems(models.Model):
+    order = models.ForeignKey(CartOrder, on_delete=models.CASCADE)
+    product_status = models.CharField(choices=STATUS_CHOICES,max_length=30,default='proccessing')
+    item = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    quantity = models.IntegerField(default=0)
+    total = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    
+    class Meta:
+        verbose_name = 'cart order item'
+        verbose_name_plural = 'cart order items'
+        
+    def __str__(self):
+        return self.order.user.username
+    
+    def order_image(self):
+        return mark_safe('<img src="/media/%s" width="50" height="50" />' % (self.image))
+    
+    
+    
     
