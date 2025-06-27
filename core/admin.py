3043e14ccf77dded4bb6580067b892c729fe3dfe
ChehAchraf <achrafchehboun@outlook.com>
2025-06-27 22:42:17 +0100

    update some models

--- core/admin.py
+++ core/admin.py
@@ -13,28 +13,29 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['title','image']
     list_filter = ['title','image']
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
 class ProductAdmin(admin.ModelAdmin):
+    
     list_display = ['title','image']
     search_fields = ['title','image']
     list_filter = ['title','image']
+    inlines = [ProductImageInline]
 
 class CartOrderAdmin(admin.ModelAdmin):
     list_display = ['user','price']
     search_fields = ['user','price']
     list_filter = ['user','price']
+    
 
-
-
-
-
-
-
-
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['product']
 
 
 admin.site.register(vendor,VendorAdmin)
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(CartOrder,CartOrderAdmin)
-admin.site.register(ProductImage)
+admin.site.register(ProductImage, ProductImageAdmin)
 
--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from shortuuid.django_fields import ShortUUIDField
 from django.utils.html import mark_safe
 from django.contrib.auth.models import User
 from django.conf import settings
+
 # Create your models here.
 
 
@@ -111,7 +112,23 @@ class Product(models.Model):
     def get_percentage(self):
         new_price = (self.old_price - self.price)  * 100
         return new_price
-    
+
+class ProductReview(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL ,null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL ,null=True)
+    review = models.TextField()
+    rating = models.IntegerField(choices=RATING , default = None)
+    date = models.DateTimeField(auto_now_add = True)
+
+    class Meta :
+        verbose_name_plural = "Product Reviews"
+
+    def __str__(self):
+        return self.product.title
+    def get_rating(self):
+        return self.rating
+
+
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='product_images')
@@ -124,7 +141,7 @@ class ProductImage(models.Model):
 
 class CartOrder(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     paid_status = models.BooleanField(default=False)
     order_date = models.DateTimeField(auto_now_add=True)
     product_status = models.CharField(choices=STATUS_CHOICES,max_length=30,default='proccessing')
@@ -143,7 +160,7 @@ class CartOrderItems(models.Model):
     image = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     quantity = models.IntegerField(default=0)
-    total = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    total = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
     
     class Meta:
         verbose_name = 'cart order item'
